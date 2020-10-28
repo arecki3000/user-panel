804a6c4e46473a2da8abb4199e117908d84a93bb
Arkadiusz Kwiatkowski <58771611+arecki3000@users.noreply.github.com>
2020-10-28 14:47:49 +0100

    finished jumbo

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ export default function App() {
       <Nav />
       <PageWrapper>
         <Jumbotron />
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
       </PageWrapper>
     </div>
   );
--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import FacebookToken from "../imgs/Facebook_Token.svg";
+import EmptyToken from "../imgs/Empty_Token.svg";
+import Verified from "../imgs/Verified.svg";
+import MsgButton from "../imgs/MsgButton.svg";
+import SpcButton from "../imgs/SpcButton.svg";
 
 const JumbotronWrapper = styled.div`
   display: grid;
   grid-template-rows: 224px 118px 33px 108px;
   grid-template-columns: 1fr 151px 1fr;
   width: 100%;
-  height: 478px;
+  min-height: 478px;
   background-color: #ffffff;
   box-shadow: 0 0 40px rgba(49, 92, 173, 0.12);
   border-radius: 25px;
-  @media (max-width: 768px) {
+  @media (max-width: 1030px) {
     grid-template-rows: 184px 118px 33px 1fr 1fr;
+  }
+  @media (max-width: 768px) {
+    grid-template-rows: 184px 118px 33px 1fr 1fr 16px;
   }
 `;
 
@@ -33,7 +41,6 @@ const AvatarContainer = styled.div`
   grid-column: 2 / 3;
   background: #ffffff;
   border-radius: 50%;
-  z-index: 1;
 `;
 
 const Avatar = styled.div`
@@ -51,20 +58,23 @@ const QuoteContainer = styled.div`
   grid-column: 1 / 2;
   align-items: flex-end;
   text-align: left;
-  padding: 27px 39px;
+  padding: 27px 36px;
   font-family: "Proxima Nova - Semibold";
   font-size: 16px;
   font-weight: 400;
   color: #ffffff;
-  @media (max-width: 768px) {
-    grid-row: 5 / 6;
+  @media (max-width: 1030px) {
+    grid-row: 4 / 5;
     grid-column: 1 / 4;
     text-align: center;
     padding: 0;
     justify-content: center;
-    align-items: flex-start;
+    align-items: flex-end;
     color: #3e495e;
   }
+  @media (max-width: 768px) {
+    grid-row: 4 / 6;
+    align-items: center;
 `;
 
 const UserName = styled.div`
@@ -79,6 +89,14 @@ const UserName = styled.div`
     font-size: 27px;
     padding-bottom: 10px;
     font-family: "Proxima Nova - Semibold";
+    display: flex;
+    align-items: center;
+    img {
+      margin-left: 6px;
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+    }
   }
   .city {
     color: #929cae;
@@ -86,15 +104,74 @@ const UserName = styled.div`
     font-family: "Poppins-Regular";
     line-height: 22px;
   }
-  @media (max-width: 768px) {
+  @media (max-width: 1030px) {
     padding: 10px;
     grid-row: 4 / 5;
   }
 `;
 
-const Tokens = styled.div``;
+const Tokens = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  grid-row: 3 / 5;
+  grid-column: 1 / 2;
+  padding-left: 36px;
+  img {
+    width: 41px;
+    height: 41px;
+    padding-right: 19px;
+  }
+  @media (max-width: 1030px) {
+    grid-row: 5 / 6;
+    grid-column: 1 / 4;
+    img {
+      width: 25px;
+      height: 25px;
+      padding-right: 19px;
+    }
+  }
+  @media (max-width: 500px) {
+    grid-row: 5 / 6;
+    justify-content: center;
+    align-items: flex-end;
+  }
+`;
 
-const Buttons = styled.div``;
+const Buttons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  grid-row: 3 / 5;
+  grid-column: 3 / 4;
+  padding: 36px;
+  img {
+    height: 59px;
+    padding-left: 21px;
+  }
+  @media (max-width: 1199px) {
+    img {
+      height: 45px;
+      padding-left: 19px;
+    }
+  }
+  @media (max-width: 1030px) {
+    grid-row: 5 / 6;
+    grid-column: 1 / 4;
+    img {
+      width: 120px;
+      height: 36px;
+      padding-left: 19px;
+    }
+  }
+  @media (max-width: 500px) {
+    grid-row: 5 / 6;
+    justify-content: space-between;
+    img {
+      padding: 0;
+    }
+  }
+`;
 
 export const Jumbotron = () => {
   return (
@@ -105,16 +182,28 @@ export const Jumbotron = () => {
         <Avatar />
       </AvatarContainer>
 
-      <Tokens />
+      <Tokens>
+        <img src={FacebookToken} alt="token" />
+        <img src={EmptyToken} alt="token" />
+        <img src={EmptyToken} alt="token" />
+        <img src={EmptyToken} alt="token" />
+      </Tokens>
       <UserName>
-        <div className="name">Imię Nazwisko</div>
+        <div className="name">
+          Imię Nazwisko
+          <img src={Verified} alt="" />
+        </div>
+
         <div className="city">San Francisko, CA</div>
       </UserName>
       <QuoteContainer>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint,
         accusamus?
       </QuoteContainer>
-      <Buttons />
+      <Buttons>
+        <img src={SpcButton} alt="SpcButton" />
+        <img src={MsgButton} alt="MsgButton" />
+      </Buttons>
     </JumbotronWrapper>
   );
 };
